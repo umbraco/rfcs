85cbd8dfbf37eb7782bb748e0a141844834cd5fe
Niels Lyngsø <nsl@umbraco.dk>
2019-06-26 16:02:08 +0200

    made tests

---
+++ concept-for-new-rich-text-editor/tests/0002/index_files/attachments.js
@@ -0,0 +1,61 @@
+(function() {
+  var HOST = "https://d13txem1unpe48.cloudfront.net/"
+
+  addEventListener("trix-attachment-add", function(event) {
+    if (event.attachment.file) {
+      uploadFileAttachment(event.attachment)
+    }
+  })
+
+  function uploadFileAttachment(attachment) {
+    uploadFile(attachment.file, setProgress, setAttributes)
+
+    function setProgress(progress) {
+      attachment.setUploadProgress(progress)
+    }
+
+    function setAttributes(attributes) {
+      attachment.setAttributes(attributes)
+    }
+  }
+
+  function uploadFile(file, progressCallback, successCallback) {
+    var key = createStorageKey(file)
+    var formData = createFormData(key, file)
+    var xhr = new XMLHttpRequest()
+
+    xhr.open("POST", HOST, true)
+
+    xhr.upload.addEventListener("progress", function(event) {
+      var progress = event.loaded / event.total * 100
+      progressCallback(progress)
+    })
+
+    xhr.addEventListener("load", function(event) {
+      if (xhr.status == 204) {
+        var attributes = {
+          url: HOST + key,
+          href: HOST + key + "?content-disposition=attachment"
+        }
+        successCallback(attributes)
+      }
+    })
+
+    xhr.send(formData)
+  }
+
+  function createStorageKey(file) {
+    var date = new Date()
+    var day = date.toISOString().slice(0,10)
+    var name = date.getTime() + "-" + file.name
+    return [ "tmp", day, name ].join("/")
+  }
+
+  function createFormData(key, file) {
+    var data = new FormData()
+    data.append("key", key)
+    data.append("file", file)
+    data.append("Content-Type", file.type)
+    return data
+  }
+})();
